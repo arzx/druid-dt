1e6052f34b2d7d872e2fb1dd65af7039ee964464
Artur Ganzha <arturganzha10@gmail.com>
2024-01-31 23:21:48 +0100

    entropy

---
+++ druid-dt.py
@@ -0,0 +1,57 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+
+
+class Druid:
+    def __init__(self, max_depth, min_examples) -> None:
+        self.max_depth = max_depth
+        self.min_examples = min_examples
+
+    def entropy(self, class_probabilities:list):
+        return sum([-p_i * np.log2(p_i) for p_i in class_probabilities if p > 0])
+
+    def information_gain():
+        pass
+
+    def gini_index(p):
+        return 1 - sum([p_i ** 2 for p_i in p])
+    
+    def class_probabilities():
+        pass
+
+    def data_entropy():
+        pass
+
+    def partition_entropy():
+        pass
+
+    def split():
+        pass
+
+    def find_best_split():
+        pass
+
+    def find_label_probabilities():
+        pass
+    
+    def create_tree():
+        pass
+
+    def pred_one_sample():
+        pass
+
+    def train():
+        pass
+
+    def predict_p():
+        pass
+
+    def predict():
+        pass
+
+    def print_tree():
+        pass
+
+    def print_recursive():
+        pass
